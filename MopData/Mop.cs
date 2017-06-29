b3a91c0705ba656b13b56c749734092b7255bb54
Wrg <xcomwrg@gmail.com>
2017-06-29 15:07:37 +0800

    add check cookies is vaild

--- MopData/Mop.cs
+++ MopData/Mop.cs
@@ -19,20 +19,22 @@ namespace MopData
                     Proxy = null,
                     Url =
                         new Uri(
-                            "http://112.5.185.82:8881/MBossWeb/mbop/index_hidden.jsp?vc=805691021746849026205712&remurl=http%3A%2F%2F112.5.185.82%3A8881%2FMBossWeb&localpre=file%3A%2Fdata%2Fdata%2Fcom.newland.mbop%2Ffiles%2Fwebcache%2F&pid=128110&pmid=18859235646&ptid=770489400020&hc=592&sm=1&sw=480&sh=800&enc=utf-8&fastmode=0&fc=89100123&url=%5Bhttp%5Dpage-fj%2Fcrm%2F4Assamble%2F4_assamble.jsp&theme=&randCode=1A2B3C4D5E6F7G8H&isVirtualXML=false&menuName=%E6%99%BA%E8%83%BD%E8%90%A5%E9%94%80&portal_id=101704&op_home_country=206&opType=0&exturl=mode%3D0%26cancel_query%3Dfalse"),
+                            "http://112.5.185.82:8881/MBossWeb/mbop/index_hidden.jsp?vc=372413391824896726205712&remurl=http%3A%2F%2F112.5.185.82%3A8881%2FMBossWeb&localpre=file%3A%2Fdata%2Fdata%2Fcom.newland.mbop%2Ffiles%2Fwebcache%2F&pid=128110&pmid=18859235646&ptid=770489400020&hc=592&sm=1&sw=480&sh=800&enc=utf-8&fastmode=0&fc=89100123&url=%5Bhttp%5Dpage-fj%2Fcrm%2F4Assamble%2F4_assamble.jsp&theme=&randCode=1A2B3C4D5E6F7G8H&isVirtualXML=false&menuName=%E6%99%BA%E8%83%BD%E8%90%A5%E9%94%80&portal_id=101704&op_home_country=206&opType=0&exturl=mode%3D0"),
                     CookieContainer = new CookieContainer()
                 };
                 Http.RequestContentType = "application/x-www-form-urlencoded";
-                Http.Post();
-                
+                Response = Http.Post();
+                if (Response.Content == string.Empty)
+                    IsEffective = true;
             }
         }
 
         public Http Http { get; set; }
         public HttpWebRequest Request { get; set; }
-        public IRestResponse Response { get; set; }
+        public HttpResponse Response { get; set; }
         public string UserId { get; } = string.Empty;
         public string Mobile { get; }
+        public bool IsEffective { get; private set; }
 
         public HttpResponse GetBaseInfo()
         {

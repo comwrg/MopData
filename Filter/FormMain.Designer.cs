3a33d3a1aac231e3d186478cbd0481e2fcd1fb2c
Wrg <xcomwrg@gmail.com>
2017-08-08 19:06:48 +0800

    add filter
    to filter database

---
+++ Filter/FormMain.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace Filter
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_attribution = new System.Windows.Forms.TextBox();
+            this.txt_package = new System.Windows.Forms.TextBox();
+            this.txt_userStatus = new System.Windows.Forms.TextBox();
+            this.txt_beginTime = new System.Windows.Forms.TextBox();
+            this.txt_billingType = new System.Windows.Forms.TextBox();
+            this.txt_userInfo = new System.Windows.Forms.TextBox();
+            this.txt_latestBindingTime = new System.Windows.Forms.TextBox();
+            this.txt_recommendInfo = new System.Windows.Forms.TextBox();
+            this.txt_businessInfo = new System.Windows.Forms.TextBox();
+            this.btn_filter = new System.Windows.Forms.Button();
+            this.txt_flow = new System.Windows.Forms.TextBox();
+            this.txt_billing = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 273);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "归属:\r\n\r\n在用套餐:\r\n\r\n用户状态:\r\n\r\n开打时间:\r\n\r\n出账类型:\r\n\r\n集团信息:\r\n\r\n最晚捆绑时间:\r\n\r\n业务信息:\r\n\r\n推荐信息:\r\n\r\n" +
+    "消费:\r\n\r\n流量:";
+            // 
+            // txt_attribution
+            // 
+            this.txt_attribution.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txt_attribution.Location = new System.Drawing.Point(100, 6);
+            this.txt_attribution.Name = "txt_attribution";
+            this.txt_attribution.Size = new System.Drawing.Size(501, 20);
+            this.txt_attribution.TabIndex = 1;
+            // 
+            // txt_package
+            // 
+            this.txt_package.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txt_package.Location = new System.Drawing.Point(100, 32);
+            this.txt_package.Name = "txt_package";
+            this.txt_package.Size = new System.Drawing.Size(501, 20);
+            this.txt_package.TabIndex = 2;
+            // 
+            // txt_userStatus
+            // 
+            this.txt_userStatus.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txt_userStatus.Location = new System.Drawing.Point(100, 58);
+            this.txt_userStatus.Name = "txt_userStatus";
+            this.txt_userStatus.Size = new System.Drawing.Size(501, 20);
+            this.txt_userStatus.TabIndex = 3;
+            // 
+            // txt_beginTime
+            // 
+            this.txt_beginTime.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txt_beginTime.Location = new System.Drawing.Point(100, 84);
+            this.txt_beginTime.Name = "txt_beginTime";
+            this.txt_beginTime.Size = new System.Drawing.Size(501, 20);
+            this.txt_beginTime.TabIndex = 4;
+            // 
+            // txt_billingType
+            // 
+            this.txt_billingType.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txt_billingType.Location = new System.Drawing.Point(100, 110);
+            this.txt_billingType.Name = "txt_billingType";
+            this.txt_billingType.Size = new System.Drawing.Size(501, 20);
+            this.txt_billingType.TabIndex = 5;
+            // 
+            // txt_userInfo
+            // 
+            this.txt_userInfo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txt_userInfo.Location = new System.Drawing.Point(100, 136);
+            this.txt_userInfo.Name = "txt_userInfo";
+            this.txt_userInfo.Size = new System.Drawing.Size(501, 20);
+            this.txt_userInfo.TabIndex = 6;
+            // 
+            // txt_latestBindingTime
+            // 
+            this.txt_latestBindingTime.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txt_latestBindingTime.Location = new System.Drawing.Point(100, 162);
+            this.txt_latestBindingTime.Name = "txt_latestBindingTime";
+            this.txt_latestBindingTime.Size = new System.Drawing.Size(501, 20);
+            this.txt_latestBindingTime.TabIndex = 7;
+            // 
+            // txt_recommendInfo
+            // 
+            this.txt_recommendInfo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txt_recommendInfo.Location = new System.Drawing.Point(100, 214);
+            this.txt_recommendInfo.Name = "txt_recommendInfo";
+            this.txt_recommendInfo.Size = new System.Drawing.Size(501, 20);
+            this.txt_recommendInfo.TabIndex = 9;
+            this.txt_recommendInfo.Text = "2017年存量客户价值提升;2017年存量客户价值提升";
+            // 
+            // txt_businessInfo
+            // 
+            this.txt_businessInfo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txt_businessInfo.Location = new System.Drawing.Point(100, 188);
+            this.txt_businessInfo.Name = "txt_businessInfo";
+            this.txt_businessInfo.Size = new System.Drawing.Size(501, 20);
+            this.txt_businessInfo.TabIndex = 8;
+            // 
+            // btn_filter
+            // 
+            this.btn_filter.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_filter.Location = new System.Drawing.Point(228, 292);
+            this.btn_filter.Name = "btn_filter";
+            this.btn_filter.Size = new System.Drawing.Size(75, 23);
+            this.btn_filter.TabIndex = 10;
+            this.btn_filter.Text = "筛选";
+            this.btn_filter.UseVisualStyleBackColor = true;
+            this.btn_filter.Click += new System.EventHandler(this.btn_filter_Click);
+            // 
+            // txt_flow
+            // 
+            this.txt_flow.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txt_flow.Location = new System.Drawing.Point(100, 266);
+            this.txt_flow.Name = "txt_flow";
+            this.txt_flow.Size = new System.Drawing.Size(501, 20);
+            this.txt_flow.TabIndex = 12;
+            this.txt_flow.Text = "1-100+;2-200+";
+            // 
+            // txt_billing
+            // 
+            this.txt_billing.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txt_billing.Location = new System.Drawing.Point(100, 240);
+            this.txt_billing.Name = "txt_billing";
+            this.txt_billing.Size = new System.Drawing.Size(501, 20);
+            this.txt_billing.TabIndex = 11;
+            this.txt_billing.Text = "1-100+;2-200+";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(613, 488);
+            this.Controls.Add(this.txt_flow);
+            this.Controls.Add(this.txt_billing);
+            this.Controls.Add(this.btn_filter);
+            this.Controls.Add(this.txt_recommendInfo);
+            this.Controls.Add(this.txt_businessInfo);
+            this.Controls.Add(this.txt_latestBindingTime);
+            this.Controls.Add(this.txt_userInfo);
+            this.Controls.Add(this.txt_billingType);
+            this.Controls.Add(this.txt_beginTime);
+            this.Controls.Add(this.txt_userStatus);
+            this.Controls.Add(this.txt_package);
+            this.Controls.Add(this.txt_attribution);
+            this.Controls.Add(this.label1);
+            this.Name = "FormMain";
+            this.Text = "筛选";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_attribution;
+        private System.Windows.Forms.TextBox txt_package;
+        private System.Windows.Forms.TextBox txt_userStatus;
+        private System.Windows.Forms.TextBox txt_beginTime;
+        private System.Windows.Forms.TextBox txt_billingType;
+        private System.Windows.Forms.TextBox txt_userInfo;
+        private System.Windows.Forms.TextBox txt_latestBindingTime;
+        private System.Windows.Forms.TextBox txt_recommendInfo;
+        private System.Windows.Forms.TextBox txt_businessInfo;
+        private System.Windows.Forms.Button btn_filter;
+        private System.Windows.Forms.TextBox txt_flow;
+        private System.Windows.Forms.TextBox txt_billing;
+    }
+}
+
---
+++ Filter/FormMain.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using Tranfer;
+using Microsoft.Office.Interop.Excel;
+
+namespace Filter
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private string black = File.ReadAllText("./black.txt");
+        private void btn_filter_Click(object sender, EventArgs e)
+        {
+            string sql = @"SELECT * FROM baseinfo
+            JOIN businessinfo  ON baseinfo.手机号 = businessinfo.手机号
+            JOIN consumeinfo   ON baseinfo.手机号 = consumeinfo.手机号
+            JOIN recommendinfo ON baseinfo.手机号 = recommendinfo.手机号";
+            List<string> list = new List<string>();
+            if (txt_attribution.Text != string.Empty)
+            {
+                list.Add($"归属='{txt_attribution.Text}'");
+            }
+            if (txt_package.Text != string.Empty)
+            {
+                list.Add($"在用套餐='{txt_package.Text}'");
+            }
+            if (txt_userStatus.Text != string.Empty)
+            {
+                list.Add($"用户状态='{txt_userStatus.Text}'");
+            }
+            if (txt_beginTime.Text != string.Empty)
+            {
+
+            }
+            if (txt_billingType.Text != string.Empty)
+            {
+                list.Add($"出账类型='{txt_billingType.Text}'");
+            }
+            if (txt_userInfo.Text != string.Empty)
+            {
+                list.Add($"集团信息='{txt_userInfo.Text}'");
+            }
+            if (txt_latestBindingTime.Text != string.Empty)
+            {
+                list.Add($"最晚捆绑时间<'{txt_latestBindingTime.Text}'");
+            }
+            if (txt_businessInfo.Text != string.Empty)
+            {
+                list.Add($"业务信息='{txt_businessInfo.Text}'");
+            }
+            if (txt_recommendInfo.Text != string.Empty)
+            {
+                string[] arr = txt_recommendInfo.Text.Split(';');
+                foreach (string s in arr)
+                {
+                    list.Add($"推荐信息 LIKE '%{s}%'");
+                }
+            }
+
+            if (txt_billing.Text != string.Empty)
+            {
+                list.AddRange(Tranfer(txt_billing.Text, "消费"));
+            }
+            if (txt_flow.Text != string.Empty)
+            {
+                list.AddRange(Tranfer(txt_flow.Text, "流量"));
+            }
+
+            sql += $" WHERE {string.Join(" AND ", list.ToArray())}";
+            Console.WriteLine(sql);
+
+            MysqlHelper mysql = new MysqlHelper();
+            var cmd = mysql.Conn.CreateCommand();
+            cmd.CommandText = sql;
+            var read = cmd.ExecuteReader();
+            int column = 1;
+            //            Microsoft.Office.Interop.Excel.Application xlApp = new Microsoft.Office.Interop.Excel.Application();
+            //            xlApp.Visible = true;
+            //            Workbook wb = xlApp.Workbooks.Add(XlWBATemplate.xlWBATWorksheet);
+            //            Worksheet ws = wb.Worksheets[1];
+
+
+            using (StreamWriter sw = new StreamWriter($"./{DateTime.Now:yyyyMMdd.hh.mm.ss}.txt", true, Encoding.Default))
+            {
+                sw.WriteLine($"手机号\t姓名\t归属\t在用套餐\t用户状态\t开打时间\t出账类型\t集团信息\t最晚捆绑时间\t业务信息\t一月消费\t一月流量\t二月消费\t二月流量\t三月消费\t三月流量\t四月消费\t四月流量\t五月消费\t五月流量\t六月消费\t六月流量\t七月消费\t七月流量\t八月消费\t八月流量\t九月消费\t九月流量\t十月消费\t十月流量\t十一月消费\t十一月流量\t十二月消费\t十二月流量\t推荐信息");
+                while (read.Read())
+                {
+                    list.Clear();
+                    string mobile = string.Empty;
+                    if (black.Contains(mobile))
+                        continue;
+                    for (int i = 0; i < read.FieldCount; i++)
+                    {
+                        string str = read[i].ToString();
+                        if (i == 0)
+                        {
+                            mobile = str;
+                        }
+                        if (str != mobile || i == 0)
+                        {
+                            list.Add(str.Replace('\n', ' '));
+                        }
+                    }
+                    sw.WriteLine(string.Join("\t", list.ToArray()));
+                }
+            }
+        }
+
+        private List<string> Tranfer(string txt, string suffix)
+        {
+            var mc = Regex.Matches(txt, "(\\d{1,2})-(\\d+)([\\+-])");
+            string[] MONTH = { "一", "二", "三", "四", "五", "六", "七", "八", "九", "十", "十一", "十二" };
+            List<string> list = new List<string>();
+            foreach (Match m in mc)
+            {
+                int month = int.Parse(m.Groups[1].Value);
+                string value = m.Groups[2].Value;
+                string flag = m.Groups[3].Value;
+
+                string sMonth = MONTH[month - 1];
+                string sFlag = null;
+                if (flag == "+")
+                    sFlag = ">";
+                else if (flag == "-")
+                    sFlag = "<";
+
+                 list.Add($"{sMonth}月{suffix}{sFlag}{value}");
+            }
+            return list;
+        }
+    }
+
+
+}
